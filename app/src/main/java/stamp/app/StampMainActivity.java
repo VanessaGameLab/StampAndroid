c6d1c0578a701d8406f859b9333eb84ee2ebac55
ianpurton <ian.purton@gmail.com>
2014-05-13 17:05:01 +0200

    Add DetermenisticKey from BitcoinJ and also Bip39.

--- app/src/main/java/stamp/app/StampMainActivity.java
+++ app/src/main/java/stamp/app/StampMainActivity.java
@@ -1,6 +1,7 @@
 package stamp.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +9,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.crypto.DeterministicKey;
+import com.google.bitcoin.crypto.HDKeyDerivation;
+import com.google.bitcoin.crypto.MnemonicCode;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class StampMainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -22,10 +33,45 @@ public class StampMainActivity extends ActionBarActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stamp_main);
 
+        String walletSeed = getWalletSeed();
+
         scanButton = (Button) findViewById(R.id.scan_button);
         scanButton.setOnClickListener(this);
 
         editText = (EditText) findViewById(R.id.editText);
+
+        editText.setText(walletSeed);
+    }
+
+    private String getWalletSeed() {
+        String walletSeed = getSharedPreferences("bip39", MODE_PRIVATE)
+                .getString("wallet-seed", "");
+
+        if(walletSeed.equals("")) {
+
+            SecureRandom sr = new SecureRandom();
+            byte[] rndBytes = new byte[32];
+            sr.nextBytes(rndBytes);
+
+            try {
+                MnemonicCode mc = new MnemonicCode();
+                List<String> mn = mc.toMnemonic(rndBytes);
+                StringBuilder seed = new StringBuilder();
+                for(String s : mn) {
+                    seed.append(s);
+                    seed.append(" ");
+                }
+
+                walletSeed = seed.toString().trim();
+
+                SharedPreferences.Editor e = getSharedPreferences("bip39", MODE_PRIVATE).edit();
+                e.putString("wallet-seed", seed.toString().trim());
+                e.commit();
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return walletSeed;
     }
 
 
@@ -65,7 +111,36 @@ public class StampMainActivity extends ActionBarActivity implements View.OnClick
                 // The Intent's data Uri identifies which contact was selected.
                 editText.setText(data.getStringExtra("result"));
                 // Do something with the contact here (bigger example below)
+                processQRRequest(data.getStringExtra("result"));
             }
+        }
+    }
+
+    private void processQRRequest(String data) {
+
+        try {
+
+            String[] params = data.split("|");
+            if(params.length < 3)
+                return;
+
+
+            String cmd = params[0];
+            //String service = params[1];
+            //String post_back = params[2];
+
+            if(cmd.equals("mpk")) {
+                String[] seed = getWalletSeed().split(" ");
+                MnemonicCode mc = new MnemonicCode();
+                byte[] rnd = mc.toEntropy(Arrays.asList(seed));
+
+                DeterministicKey ekprv = HDKeyDerivation.createMasterPrivateKey(rnd);
+
+                editText.setText(ekprv.serializePubB58());
+            }
+        }
+        catch(Exception e) {
+            // TODO
         }
     }
 }
